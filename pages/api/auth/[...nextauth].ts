6fac9f4ddc80ba798d08fd26e7c3701a5c99d83f
with-heart <with.heart+git@pm.me>
2022-05-20 15:21:05 -0400

    add token refresh

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,4 +1,5 @@
 import NextAuth from 'next-auth'
+import {JWT} from 'next-auth/jwt'
 import DiscordProvider from 'next-auth/providers/discord'
 
 const scopes = ['identify'].join(' ')
@@ -33,15 +34,62 @@ export default NextAuth({
         }
       }
 
-      return token
+      // return existing token because it's not expired
+      if (Date.now() < (token.accessTokenExpires as number)) {
+        return token
+      }
+
+      // token expired so try to refresh it
+      return refreshDiscordToken(token)
     },
     session({session, token}) {
       // @ts-expect-error
       session.user = token.user
-      session.accessToken = token.accessToken
-      session.error = token.error
+      session.accessToken = token.accessToken as string
+      session.error = token.error as string | undefined
 
       return session
     },
   },
 })
+
+async function refreshDiscordToken(token: JWT) {
+  try {
+    const url = 'https://discord.com/api/oauth2/token'
+    const body = new URLSearchParams({
+      client_id: process.env.DISCORD_CLIENT_ID!,
+      client_secret: process.env.DISCORD_CLIENT_SECRET!,
+      grant_type: 'refresh_token',
+      refresh_token: token.refreshToken as string,
+    }).toString()
+
+    const response = await fetch(url, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body,
+      method: 'POST',
+    })
+
+    const refreshedTokens = await response.json()
+
+    if (!response.ok) {
+      throw refreshedTokens
+    }
+
+    return {
+      ...token,
+      accessToken: refreshedTokens.access_token,
+      accessTokenExpires:
+        Date.now() + (refreshedTokens.expires_in as number) * 1000,
+      refreshToken: refreshedTokens.refresh_token ?? token.refreshToken,
+    }
+  } catch (error) {
+    console.error(error)
+
+    return {
+      ...token,
+      error: 'RefreshAccessTokenError',
+    }
+  }
+}
